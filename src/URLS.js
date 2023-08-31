77f38a915a581b89c4187c84c8a413f090646beb
poornesh h n <poorneshhn3@gmail.com>
2023-09-01 00:52:10 +0530

    fix redirect url mistake

--- src/URLS.js
+++ src/URLS.js
@@ -1,6 +1,6 @@
 module.exports = {
   // client_url: "http://localhost:3000",
   // callback_url: "http://localhost:5000",
-  redirect_url: "https://emaily-p.vercel.app",
+  client_url: "https://emaily-p.vercel.app",
   callback_url: "https://z4a6d0286-zf62858b5-gtw.zb20e5e48.qovery.fr",
 };
